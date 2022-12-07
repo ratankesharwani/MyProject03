36afd789945a229cacee4861afb378d1f3b03013
ratan <ratan.kesharwani@rationalfx.com>
2022-12-07 17:55:03 +0530

    Authentications and guard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'MyProject03'`, () => {
+  it(`should have as title 'project5'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MyProject03');
+    expect(app.title).toEqual('project5');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('MyProject03 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('project5 app is running!');
   });
 });
